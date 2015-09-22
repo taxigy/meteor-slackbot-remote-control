06c953e55679426bd89b9295f319d089e0bb8272
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2015-09-23 02:05:16 +0400

    init

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+    name: 'taxigy:slackbot-remote-control',
+    version: '0.0.1',
+    summary: 'A Meteor package that wraps Slack basic feature, Slackbot remote control, that allows posting messages into any channel on behalf of @slackbot.',
+    git: 'https://github.com/taxigy/meteor-slackbot-remote-control',
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.0.1');
+    api.use('ecmascript');
+    api.use('http');
+    api.addFiles('slackbot-remote-control.js');
+
+    if (api.export) {
+        api.export('Slackbot');
+    }
+});
+
+Package.onTest(function(api) {
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('http');
+    api.use('taxigy:slackbot-remote-control');
+    api.addFiles('slackbot-remote-control-tests.js');
+});
+
---
+++ slackbot-remote-control.js
@@ -0,0 +1,31 @@
+Slackbot = function (options) {
+};
+
+Slackbot.post = function (channel, message) {
+    var url;
+    var token;
+
+    if (!Meteor.settings.slack) {
+        throw new Meteor.Error(500, 'No Slack settings provided.');
+    }
+
+    url = Meteor.settings.slack.url;
+    token = Meteor.settings.slack.token;
+
+    if (!url) {
+        throw new Meteor.Error(404, 'No URL provided for Slackbot.');
+    }
+
+    if (!token) {
+        throw new Meteor.Error(401, 'No token provided for Slackbot.');
+    }
+
+    HTTP.post(url, {
+        params: {
+            token: String(token),
+            channel: String(channel)
+        },
+        data: message
+    });
+};
+
